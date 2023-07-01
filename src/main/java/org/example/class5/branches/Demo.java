322a32d1c43cb95b337072f854b21adeb971b3f7
Serg Zhe <polwen@yandex.ru>
2023-07-01 17:50:06 +0300

    add branch example

---
+++ src/main/java/org/example/class5/branches/Demo.java
@@ -0,0 +1,9 @@
+package org.example.class5.branches;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        // todo
+        System.out.println("Hello from branch demo");
+    }
+}
